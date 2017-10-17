900a7bf3fcb27c61ad10367c807c5853431d0f0c
Mauricio Amorim <mjvamorim@gmail.com>
2017-10-17 20:23:04 -0200

    Convidados Controller/Model/Repository/View Inicial

---
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -0,0 +1,27 @@
+package com.algaworks.festa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.festa.model.Convidado;
+import com.algaworks.festa.repository.Convidados;
+
+@Controller
+public class ConvidadosController {
+	
+	@Autowired
+	private Convidados convidados;
+	
+	@GetMapping("/convidados")
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView("ListaConvidados");
+		List <Convidado> lista = convidados.findAll();
+		mv.addObject("convidados",lista);
+		return mv;
+	}
+
+}
