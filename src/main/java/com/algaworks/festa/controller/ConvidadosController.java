eebaae4f7db0099cbcaa28927d9377b5e0acd00e
Mauricio Amorim <mjvamorim@gmail.com>
2017-11-07 19:57:34 -0200

    Alteração e Exclusão

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -5,19 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.festa.model.Convidado;
 import com.algaworks.festa.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
-	
+
 	@Autowired
 	private Convidados convidados;
-	
-	@GetMapping("/convidados")
+
+	@GetMapping()
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("ListaConvidados");
 		List <Convidado> lista = convidados.findAll();
@@ -26,12 +30,31 @@ public class ConvidadosController {
 		return mv;
 	}
 
-	@PostMapping("/convidados")
+	@PostMapping()
 	public String salvar(Convidado c) {
 		convidados.save(c);
 		return "redirect:/convidados";
 
 	}
 
+	//URL	de	chamada	http://.../excluir/1234				
+	@RequestMapping(value ="/excluir/{id}")
+	public String excluirConvidadoByPathVariable(@PathVariable Long id, RedirectAttributes attributes)	{
+		this.convidados.delete(id);
+		attributes.addFlashAttribute("mensagem", "Convidado excluído com sucesso!");
+		return "redirect:/convidados";
+	}
+	
+	//URL	de	chamada	http://.../alterar/1234				
+	@RequestMapping(value = "/alterar/{id}")
+	public ModelAndView alterar(@PathVariable Long id, RedirectAttributes attributes) {
+		ModelAndView mv = new ModelAndView("ListaConvidados");
+		List <Convidado> lista = convidados.findAll();
+		mv.addObject("convidados",lista);
+		Convidado convidado = convidados.findOne(id);
+		mv.addObject(convidado);
+		
+		return mv;
+	}
 
 }
